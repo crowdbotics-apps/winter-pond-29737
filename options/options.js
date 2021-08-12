e578b666904d61aab4a9461fe65af7a11f33755c
Crowdbotics <team@crowdbotics.com>
2021-08-12 00:38:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "winter_pond_29737Identifier",
+  url: "https://winter_pond_29737Identifier.botics.co",
+  api: "https://winter_pond_29737Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
